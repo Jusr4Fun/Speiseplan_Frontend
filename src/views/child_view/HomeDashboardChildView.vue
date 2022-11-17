aa5dc4872857c9ea973049cfc637354b41baa880
DvorakDoCJD <ddvorak@cjd.de>
2022-11-17 11:06:58 +0100

    erste überarbeitung home dash board

--- src/views/child_view/HomeDashboardChildView.vue
+++ src/views/child_view/HomeDashboardChildView.vue
@@ -1,96 +1,75 @@
 <template>
-  <div class="about">
-    <v-container fluid class="justify-center">
-      <v-row class="align-end">
-        <v-card width="250" height="250" class="ma-4"> kevin </v-card>
-
-        <v-card width="250" height="250" class="ma-4"> kevin </v-card>
-
-        <v-card width="250" height="250" class="ma-4"> kevin </v-card>
-
-        <v-card width="250" height="250" class="ma-4"> kevin </v-card>
-
-        <v-card width="250" height="250" class="ma-4 justify-start">
-          kevin
-        </v-card>
-
-        <v-card width="150" height="150" class="ma-4 justify-end align-end">
-          kevin
-        </v-card>
-      </v-row>
-    </v-container>
-    <!-- <v-row class="ma-4 d-flex justify-start">
+  <v-container fill-height fluid>
+    <v-row class="d-flex justify-start dense" dense>
       <v-col>
         <v-card
-          class="indigo darken-4 white--text text-center rounded-xl"
+          class="indigo darken-4 fill-height white--text text-center rounded-xl0"
           elevation="2"
           :to="'/' + 'Bestellungen'"
-          width="250"
-          height="250"
-          ><v-icon size="200" color="grey darken-1">mdi-calendar-today</v-icon
-          >Bestellung aktuelle Woche
+          ><v-icon size="75" color="grey darken-1">mdi-calendar-today</v-icon
+          ><v-card-text class="white--text"
+            >Bestellung aktuelle Woche</v-card-text
+          >
         </v-card>
       </v-col>
       <v-col>
         <v-card
-          class="orange accent-4 white--text text-center rounded-xl"
+          class="orange accent-4 fill-height white--text text-center rounded-xl0"
           elevation="2"
           :to="'/' + 'Bestellungen'"
-          width="250"
-          height="250"
-          ><v-icon size="200" color="grey darken-3">mdi-calendar-week</v-icon
-          >Bestellung bestimmte Woche</v-card
+          ><v-icon size="75" color="grey darken-3">mdi-calendar-week</v-icon
+          ><v-card-text class="white--text"
+            >Bestellung bestimmte Woche</v-card-text
+          ></v-card
         >
       </v-col>
       <v-col>
         <v-card
-          class="red accent-4 white--text text-center rounded-xl"
+          class="red accent-4 fill-height white--text text-center rounded-xl0"
           elevation="2"
           :to="'/' + 'Teilnehmer'"
-          width="250"
-          height="250"
-          ><v-icon size="200" color="grey darken-3">mdi-account-plus</v-icon
-          >Teilnehmer hinzufügen</v-card
+          ><v-icon size="75" color="grey darken-3">mdi-account-plus</v-icon
+          ><v-card-text class="white--text"
+            >Teilnehmer hinzufügen</v-card-text
+          ></v-card
         >
       </v-col>
-    </v-row>
-    <v-row class="ma-4 d-flex justify-start" cols="3">
       <v-col class="justify-start">
         <v-card
-          class="deep-purple accent-4 white--text text-center rounded-xl"
+          class="deep-purple fill-height accent-4 white--text text-center rounded-xl0"
           elevation="2"
           :to="'/' + 'Bestellungen'"
-          width="250"
-          height="250"
-          ><v-icon size="200" color="grey darken-1">mdi-account-minus</v-icon
-          >Teilnehmer entfernen</v-card
+          ><v-icon size="75" color="grey darken-1">mdi-account-minus</v-icon>
+          <v-card-text class="white--text"
+            >Teilnehmer entfernen</v-card-text
+          ></v-card
         >
       </v-col>
       <v-col class="justify-start">
         <v-card
-          class="green accent-4 white--text text-center rounded-xl"
+          class="green accent-4 fill-height white--text text-center rounded-x10"
           elevation="2"
           :to="'/' + 'GesamtBestellungen'"
-          width="250"
-          height="250"
-          ><v-icon size="200" color="grey darken-3"
-            >mdi-clipboard-text-outline</v-icon
-          >Gesammte Bestellungen anzeigen</v-card
         >
+          <v-icon size="75" color="grey darken-3">
+            mdi-clipboard-text-outline
+          </v-icon>
+          <v-card-text class="white--text">
+            Gesammte Bestellungen anzeigen
+          </v-card-text>
+        </v-card>
       </v-col>
     </v-row>
-    <v-row class="ma- 4">
+    <v-row class="fill-height" dense>
       <v-col>
         <v-card
-          class="text-center rounded-xl"
+          class="text-center green fill-height accent-4 rounded-x10"
           elevation="0"
-          :to="'/' + 'Bestellungen'"
-          width="150"
-          height="150"
+          :to="'/' + 'Benutzer'"
           ><v-icon size="130" color="grey darken-3">mdi-cog</v-icon
           >Benutzerverwaltung</v-card
         >
       </v-col>
-    </v-row> -->
-  </div>
+    </v-row>
+  </v-container>
 </template>

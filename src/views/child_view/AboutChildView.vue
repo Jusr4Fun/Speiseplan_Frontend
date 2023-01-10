70effc594b03f7f4f163408a2a086e701e1c993f
DvorakDoCJD <ddvorak@cjd.de>
2023-01-10 08:15:31 +0100

    test

--- src/views/child_view/AboutChildView.vue
+++ src/views/child_view/AboutChildView.vue
@@ -9,7 +9,8 @@
           Standort Hohenreissach <br />
           Abteilung Fachinformatiker <br />
           Alte Plochinger Steige 158 <br />
-          72320 Kirchheim unter Teck
+          72320 Kirchheim unter Teck <br />
+          Dies it ein Test
         </v-card-text>
       </v-card>
     </v-row>
